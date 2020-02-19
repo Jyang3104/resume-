46c3617d60d81a264638064d20e433842e8bec6c
Jianwen Yang <jianwenyang@Jianwens-MacBook-Pro.local>
2020-02-18 20:22:25 -0500

    create home and entry file

---
+++ server.js
@@ -0,0 +1,20 @@
+const exp=require("express");
+const app=exp();
+const exphbs=require("express-handlebars");
+const bodyParser=require("body-parser");
+
+app.engine(`handlebars`, exphbs());
+app.set(`view engine`,`handlebars`);
+
+app.get("/", (req,res)=>{
+res.render("home",{
+title:`Home`
+})
+
+const PORT=process.env.PORT||3000
+app.listen(PORT,()=>{
+
+    console.log("start server now...");
+})
+
+})
