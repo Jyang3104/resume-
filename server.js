9c920d39f33d8638698b4e9513996e178e1149dc
Jianwen Yang <jianwenyang@Jianwens-MacBook-Pro.local>
2020-02-18 20:58:23 -0500

    add skills and project in main

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const exp=require("express");
 const app=exp();
-const exphbs=require("express-handlebars");
-const bodyParser=require("body-parser");
+const exphbs=require('express-handlebars');
+const bodyParser=require('body-parser');
 
-app.engine(`handlebars`, exphbs());
-app.set(`view engine`,`handlebars`);
+app.engine('handlebars', exphbs());
+app.set('view engine','handlebars');
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.get("/", (req,res)=>{
 res.render("home",{
 title:`Home`
-})
+  })
+});
 
 const PORT=process.env.PORT||3000
 app.listen(PORT,()=>{
@@ -17,4 +19,3 @@ app.listen(PORT,()=>{
     console.log("start server now...");
 })
 
-})
